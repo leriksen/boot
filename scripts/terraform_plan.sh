6e7554232154fea5d21534e771f9a16f2fc1b7c9
Leif Eriksen <leif.eriksen.au@gmail.com>
2023-01-13 21:13:46 +1100

    remove env call

--- scripts/terraform_plan.sh
+++ scripts/terraform_plan.sh
@@ -2,3 +2,20 @@
 
 set -euo pipefail
 
+# -detailed error code give 2 as rc if plan needs an apply
+set +e
+terraform plan -detailed-exitcode "${@}" -out tfplan.tfout
+rc=$?
+set -e
+
+if [[ $rc -eq 0 ]]; then
+  echo "No diff, no apply needed"
+elif [[ $rc -eq 1 ]]; then
+  echo "Error running plan"
+  exit 1
+elif [[ $rc -eq 2 ]]; then
+  echo "Diff, apply needed"
+else
+  echo "Unexpected rc $rc - erroring"
+  exit $rc
+fi
