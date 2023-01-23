fd4aa2b9215dc9f3b8d84c9d66687b446c73c646
Leif Eriksen <leif.eriksen.au@gmail.com>
2023-01-23 13:25:30 +1100

    add child resources to adf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,3 +1,5 @@
 data "azurerm_resource_group" "rg" {
   name = "rg-iqsr-${var.env}"
 }
+
+data "azurerm_client_config" "current" {}
--- terraform/datafactory.tf
+++ terraform/datafactory.tf
@@ -1,5 +1,45 @@
+resource "azurerm_user_assigned_identity" "user1" {
+  location            = data.azurerm_resource_group.rg.location
+  name                = "uai-user1-${var.env}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_user_assigned_identity" "user2" {
+  location            = data.azurerm_resource_group.rg.location
+  name                = "uai-uniting-iqsr-ae-user2-${var.env}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
 resource "azurerm_data_factory" "example" {
   name                = "adf-iqsr-ae-${var.env}-001"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-}
+
+  identity {
+    type         = "SystemAssigned, UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.user1.id,
+      azurerm_user_assigned_identity.user2.id,
+    ]
+  }
+
+  global_parameter {
+    name  = "gp1"
+    type  = "Bool"
+    value = "false"
+  }
+
+  global_parameter {
+    name  = "gp2"
+    type  = "Bool"
+    value = "false"
+  }
+
+   vsts_configuration {
+     account_name    = "leriksen"
+     branch_name     = var.env
+     project_name    = "datascience"
+     repository_name = "datascience"
+     root_folder     = "/"
+     tenant_id       = data.azurerm_client_config.current.tenant_id
+   }}
